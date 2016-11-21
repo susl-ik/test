04b5064ccc2fe27df0b941003bb8c58b578deb4c
e.gavrilenko <e.gavrilenko@dkhz.com.ua>
2016-11-21 10:28:16 +0200

    проверка доступности в index

--- index.php
+++ index.php
@@ -61,43 +61,44 @@ echo ("<div class=\"page-header\">
 $xml = simplexml_load_file('config.xml');
    // print_r($xml);
 
-$message="";
 foreach($xml->printers->printer as $printer) {
 	echo('<div class="panel panel-primary">
   	<div class="panel-heading"><span class="glyphicon glyphicon-print"></span>');
 	//$last=$printer->last;
 	$ip = $printer->ip;
 	echo "  minolta ".$printer->id."</div><div class=\"panel-body\">";
+	
+	$fp = fsockopen ("$ip", 80, $errno, $errstr, 15);
+	if (!$fp) {
+		echo ('<div class="alert alert-danger">not available </div>');
+	} else {	
+		echo "<div>control date: <span class=\"badge\">".$printer->lastControlTime."</span></div>";
+		echo "<div>control count: <span class=\"badge\">".$printer->lastControlCount."</span></div>";
+		$info =  (int) snmpget($ip, "private", ".1.3.6.1.4.1.18334.1.1.1.5.7.2.1.1.0");
+		echo "<div>current count: <span class=\"badge\">".$info."</span></div>";
+			//echo "type: ".gettype($info)."</br>";
+		$lastdelta=$info-$printer->last;
+	//	echo "last delta: ".$lastdelta."</br>";
+		$fulldelta=$info-$printer->lastControlCount;
+		$status="success";
+		if ($fulldelta>7000) {$status="warning"; }
+		if ($fulldelta>15000) {$status="danger"; }
+		echo "<div>full delta: <span class=\"label label-".$status."\">".$fulldelta."</span></div>";
+		$printer->last=$info;	
+		//$printer->lastControlCount=$info;
+		//$printer->lastControlTime=$runtime;
 
-	echo "<div>control date: <span class=\"badge\">".$printer->lastControlTime."</span></div>";
-	echo "<div>control count: <span class=\"badge\">".$printer->lastControlCount."</span></div>";
-	$info =  (int) snmpget($ip, "private", ".1.3.6.1.4.1.18334.1.1.1.5.7.2.1.1.0");
-	echo "<div>current count: <span class=\"badge\">".$info."</span></div>";
-	$message=$message."minolta ".$printer->id." ".$info."\n";
-	//echo "type: ".gettype($info)."</br>";
-	$lastdelta=$info-$printer->last;
-//	echo "last delta: ".$lastdelta."</br>";
-	$fulldelta=$info-$printer->lastControlCount;
-	$status="success";
-	if ($fulldelta>7000) {$status="warning"; }
-	if ($fulldelta>15000) {$status="danger"; }
-	echo "<div>full delta: <span class=\"label label-".$status."\">".$fulldelta."</span></div>";
-	$printer->last=$info;	
-	//$printer->lastControlCount=$info;
-	//$printer->lastControlTime=$runtime;
+		$d1= new DateTime($printer->lastControlTime);
+		$d2= new DateTime($runtime);
+		$interval = $d1->diff($d2)->format('%R%a дней');
+		echo "interval: ".$interval."</br>";
+		
 
-	$d1= new DateTime($printer->lastControlTime);
-	$d2= new DateTime($runtime);
-	$interval = $d1->diff($d2)->format('%R%a дней');
-	echo "interval: ".$interval."</br>";
+		$printer->lasttime=$runtime;
+	}
 	echo("</div></div>");
-
-
-
-	$printer->lasttime=$runtime;
 };
 
-//$xml->printer[1]->last=2;
 $xml->asXML('config.xml');
 
 
